715f645e23d25bca4a2952442e7f40edbb0dca8f
Jason Cole <jasonacole82@gmail.com>
2018-08-13 22:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
+        newFacts.push(facts[i] + "!!!");
         i++;
     }
     return newFacts;
